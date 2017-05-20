09ac5f924d57e8f995d04bf4efec9ccc54ffcef9
balazs4 <balazs4web@gmail.com>
2017-05-20 18:13:47 +0200

    yay...ready-to-deploy version :)

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+require('mini-linq-js');
 const pingLite = require('ping-lite');
+
 const ping = target =>
   new Promise((resolve, reject) => {
     const x = new pingLite(target);
     x.send((err, ms) => {
-      resolve({ server: target, ms: ms || 100 });
+      resolve({ server: target, ms: ms || Infinity });
     });
   });
 
-const targets = [].concat
-  .apply(
-    [],
-    [...Array(40).keys()]
-      .filter(x => x % 10 === 0)
-      .map(x => x + 10)
-      .map(x => 'bcde'.split('').map(y => `${x}${y}`))
-  )
-  .map(xy => `smooth.edge${xy}.rtl.hu`);
+const servers = [10, 20, 30, 40]
+  .selectMany(number => ['b', 'c', 'd', 'e'].select(char => `${number}${char}`))
+  .select(x => `smooth.edge${x}.rtl.hu`);
 
-Promise.all(targets.map(ping))
-  .then(winner => console.log(winner))
-  .catch(err => console.error(err));
+const elect = targets =>
+  Promise.all(targets.map(ping)).then(results =>
+    results
+      .where(x => x.ms !== Infinity)
+      .orderBy(x => x.ms)
+      .select(x => x.server)
+      .firstOrDefault()
+  );
+
+module.exports = async (req, res) => {
+  const server = await elect(servers);
+  const target = `http://${server}${req.url}`;
+  console.log(target);
+  res.writeHead(302, {
+    Location: target
+  });
+  res.end();
+};
